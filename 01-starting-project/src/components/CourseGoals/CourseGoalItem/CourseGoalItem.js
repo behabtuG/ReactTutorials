090ad8a3a9c073d22030d36cef10578e8cda903a
BehabtuG <BehabtuG>
2024-03-28 09:47:51 +0300

    module 1

--- 01-starting-project/src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
+++ 01-starting-project/src/components/CourseGoals/CourseGoalItem/CourseGoalItem.js
@@ -1,17 +1,14 @@
 import React from "react";
 
-import "./CourseGoalItem.css";
+import styles from "./CourseGoalItem.module.css";
 
 const CourseGoalItem = (props) => {
-  // const [deleteText, setDeleteText] = useState('');
-
   const deleteHandler = () => {
-    // setDeleteText('(Deleted!)');
     props.onDelete(props.id);
   };
 
   return (
-    <li className="goal-item" onClick={deleteHandler}>
+    <li className={styles["goal-item"]} onClick={deleteHandler}>
       {props.children}
     </li>
   );

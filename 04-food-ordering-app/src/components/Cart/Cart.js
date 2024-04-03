bee4a91ab35fa2d52521dea65ddd169789fffb5d
BehabtuG <BehabtuG>
2024-04-03 16:19:01 +0300

    add order Modal

---
+++ 04-food-ordering-app/src/components/Cart/Cart.js
@@ -0,0 +1,28 @@
+import Modal from "../UI/Modal";
+import classes from "./Cart.module.css";
+
+const Cart = (props) => {
+  const cartitems = (
+    <ul>
+      {[{ id: "c1", name: "Shshi", amount: 2, price: 12.99 }].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+
+  return (
+    <Modal>
+      {cartitems}
+      <div className={classes.total}>
+        <span>Total mount</span>
+        <span>35.63</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button--alt"]}>CLose</button>
+        <button className={classes.button}>Order</button>
+      </div>
+    </Modal>
+  );
+};
+
+export default Cart;
